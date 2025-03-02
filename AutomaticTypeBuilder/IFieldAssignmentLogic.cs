cd109daf69efacfd5122dc0aaff3881cf67891e9
Greg Liaskas <greg.liaskas@outlook.com>
2025-03-02 04:19:18 +0200

    feat: add overloaded Initialize methods to IFieldAssignmentLogic for enhanced value generation

--- AutomaticTypeBuilder/IFieldAssignmentLogic.cs
+++ AutomaticTypeBuilder/IFieldAssignmentLogic.cs
@@ -6,4 +6,6 @@ public interface IFieldAssignmentLogic
     public IFieldAssignmentLogic When<T>(Func<T> initialize);
 
     public T? Initialize<T>();
+    public void Initialize(out IEnumerable<object> values, in IEnumerable<Type> types);
+    public void Initialize(out IEnumerable<object> values, out IEnumerable<Type> types, int fieldLimit);
 }
--- AutomaticTypeBuilder/Internals/FieldAssignmentLogic.cs
+++ AutomaticTypeBuilder/Internals/FieldAssignmentLogic.cs
@@ -8,6 +8,8 @@ internal class FieldAssignmentLogic : IFieldAssignmentLogic
     private readonly Dictionary<Type, Delegate> _customLogic = [];
     private readonly ReadOnlyDictionary<Type, Delegate> _prebuildLogic = PrebuiltData.AssignmentLogic;
     
+    private IReadOnlyCollection<Type> RegisteredTypes => [.. _customLogic.Keys.Concat(_prebuildLogic.Keys).Distinct()];
+
 
     public IFieldAssignmentLogic When<T>(Func<T> initialize)
     {
@@ -22,5 +24,20 @@ internal class FieldAssignmentLogic : IFieldAssignmentLogic
 
         _prebuildLogic.TryGetValue(typeof(T), out initialization);
         return initialization is Func<T> prebuiltInitialization ? prebuiltInitialization() : default;
+    }
+
+    public void Initialize(out IEnumerable<object> values, in IEnumerable<Type> types) => values = types.Select(type =>
+    {
+        var method = typeof(FieldAssignmentLogic).GetMethod(nameof(Initialize), Type.EmptyTypes)
+                                                 ?.MakeGenericMethod(type);
+
+        return method?.Invoke(this, null) ?? throw new InvalidOperationException("Tried to invoke a nonexistent method");
+    });
+
+    public void Initialize(out IEnumerable<object> values, out IEnumerable<Type> types, int fieldLimit)
+    {
+        var random = new Random();
+        types = [..RegisteredTypes.OrderBy(x => random.Next()).Take(fieldLimit)];
+        Initialize(out values, in types);        
     }
 }
