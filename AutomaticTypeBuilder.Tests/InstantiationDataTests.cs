ede2a24f2c34e310746f07ef6e1283c7bf6561c2
Greg Liaskas <greg.liaskas@outlook.com>
2025-03-03 09:44:42 +0200

    test: add unit tests for InstantiationData constructor and DataAt method to validate exception handling

--- AutomaticTypeBuilder.Tests/InstantiationDataTests.cs
+++ AutomaticTypeBuilder.Tests/InstantiationDataTests.cs
@@ -29,6 +29,15 @@ public class InstantiationDataTests
     }
 
     [Fact]
+    public void Ctor_ThrowsInvalidDataException_WithNegativeFieldCount()
+    {
+        MockAssignmentLogicSetup(out var mockedAssignmentLogic);
+        InstantiationData act() => new(mockedAssignmentLogic.Object, -5);
+
+        Assert.Throws<InvalidDataException>(act);
+    }    
+
+    [Fact]
     public void Count_Correctly_Returs_NumberOfFields()
     {
         MockAssignmentLogicSetup(out var mockedAssignmentLogic, 2);
@@ -49,8 +58,30 @@ public class InstantiationDataTests
         
         Assert.Equal(expected:expectedType, actual:actualInfo.Type);
         Assert.Equal(expected:expectedValue, actual:actualInfo.Value);        
-    }     
-            
+    }    
+
+    [Fact]        
+    public void DataAt_IndexOutOfRangeException_WithNegative_IndexProvided()
+    {
+        MockAssignmentLogicSetup(out var mockedAssignmentLogic, 2);
+        var instantiationData = new InstantiationData(mockedAssignmentLogic.Object, 2);
+
+        void act() => instantiationData.DataAt(-5);
+        
+        Assert.Throws<IndexOutOfRangeException>(act);        
+    }
+
+    [Fact]        
+    public void DataAt_IndexOutOfRangeException_WithGreaterThatCapacity_IndexProvided()
+    {
+        MockAssignmentLogicSetup(out var mockedAssignmentLogic, 2);
+        var instantiationData = new InstantiationData(mockedAssignmentLogic.Object, 2);
+
+        void act() => instantiationData.DataAt(instantiationData.FieldCount+5);
+        
+        Assert.Throws<IndexOutOfRangeException>(act);        
+    }
+
 
     private static void MockAssignmentLogicSetup(out Mock<IFieldAssignmentLogic> mockedAssignmentLogic,
                                                  int fieldCount = 5)
