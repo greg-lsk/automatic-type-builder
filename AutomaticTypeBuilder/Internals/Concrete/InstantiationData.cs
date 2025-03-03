3667c2c074e73f5f5ced13035332b04e99140d18
Greg Liaskas <greg.liaskas@outlook.com>
2025-03-03 09:44:25 +0200

    fix: add validation for fieldCount and index in InstantiationData

--- AutomaticTypeBuilder/Internals/Concrete/InstantiationData.cs
+++ AutomaticTypeBuilder/Internals/Concrete/InstantiationData.cs
@@ -12,11 +12,18 @@ internal class InstantiationData: IInstantiationData
 
     public InstantiationData(IFieldAssignmentLogic assignmentLogic, int fieldCount = 5)
     {
+        if(fieldCount < 0) throw new InvalidDataException();
+
         assignmentLogic.Initialize(fieldCount, out _values, out _types);
     }
 
 
     public int FieldCount => _types.Count();
 
-    public (Type Type, object? Value) DataAt(int index) => (_types.ElementAt(index), _values.ElementAt(index));
+    public (Type Type, object? Value) DataAt(int index)
+    {
+        if(index < 0 || index > _types.Count()) throw new IndexOutOfRangeException();
+
+        return (_types.ElementAt(index), _values.ElementAt(index));
+    }
 }
