0226303b4de359d746731193bf520e59ce15cd08
Greg Liaskas <greg.liaskas@outlook.com>
2025-03-03 12:03:38 +0200

    refactor: update variable names for clarity in InstantiationDataTests

--- AutomaticTypeBuilder.Tests/InstantiationDataTests.cs
+++ AutomaticTypeBuilder.Tests/InstantiationDataTests.cs
@@ -7,35 +7,35 @@ namespace AutomaticTypeBuilder.Tests;
 
 public class InstantiationDataTests
 {
-    private static readonly int _mockFieldCount = TestData.InitializationDataCount; 
-    private static IEnumerable<Type> _mockedTypes = TestData.InitializationTypes;
-    private static IEnumerable<object?> _mockedValues = TestData.InitializationValues;
+    private static readonly int _expectedFieldCount = TestData.InitializationDataCount; 
+    private static IEnumerable<Type> _expectedInitializedTypes = TestData.InitializationTypes;
+    private static IEnumerable<object?> _expectedInitializedValues = TestData.InitializationValues;
     public static TheoryData<int, Type, object?> InitializationDataAtIndex => TestData.InitializationDataAtIndex;
 
 
     [Fact]
     public void Ctor_Correctly_Initializes_Values()
     {
-        MockAssignmentLogicSetup(out var mockedAssignmentLogic, _mockFieldCount);
-        var instantiationData = new InstantiationData(mockedAssignmentLogic.Object, _mockFieldCount);
+        MockAssignmentLogicSetup(out var mockedAssignmentLogic, _expectedFieldCount);
+        var instantiationData = new InstantiationData(mockedAssignmentLogic.Object, _expectedFieldCount);
 
-        Assert.Equal(expected:_mockedValues, actual:instantiationData.Values);
+        Assert.Equal(expected:_expectedInitializedValues, actual:instantiationData.Values);
     }
 
     [Fact]
     public void Ctor_Correctly_Initializes_Types()
     {
-        MockAssignmentLogicSetup(out var mockedAssignmentLogic, _mockFieldCount);
-        var instantiationData = new InstantiationData(mockedAssignmentLogic.Object, _mockFieldCount);
+        MockAssignmentLogicSetup(out var mockedAssignmentLogic, _expectedFieldCount);
+        var instantiationData = new InstantiationData(mockedAssignmentLogic.Object, _expectedFieldCount);
 
-        Assert.Equal(expected:_mockedTypes, actual:instantiationData.Types);
+        Assert.Equal(expected:_expectedInitializedTypes, actual:instantiationData.Types);
     }
 
     [Fact]
     public void Ctor_ThrowsInvalidDataException_WithNegativeFieldCount()
     {
         MockAssignmentLogicSetup(out var mockedAssignmentLogic);
-        InstantiationData act() => new(mockedAssignmentLogic.Object, -_mockFieldCount);
+        InstantiationData act() => new(mockedAssignmentLogic.Object, _expectedFieldCount*(-1));
 
         Assert.Throws<InvalidDataException>(act);
     }    
@@ -43,18 +43,18 @@ public class InstantiationDataTests
     [Fact]
     public void Count_Correctly_Returs_NumberOfFields()
     {
-        MockAssignmentLogicSetup(out var mockedAssignmentLogic, _mockFieldCount);
-        var instantiationData = new InstantiationData(mockedAssignmentLogic.Object, _mockFieldCount);
+        MockAssignmentLogicSetup(out var mockedAssignmentLogic, _expectedFieldCount);
+        var instantiationData = new InstantiationData(mockedAssignmentLogic.Object, _expectedFieldCount);
 
-        Assert.Equal(expected:_mockedTypes.Count(), actual:instantiationData.FieldCount);
+        Assert.Equal(expected:_expectedInitializedTypes.Count(), actual:instantiationData.FieldCount);
     }
 
     [Theory]
     [MemberData(nameof(InitializationDataAtIndex))]
     public void DataAt_Correctly_Returts_FieldInfo_FromIndex(int index, Type expectedType, object? expectedValue)
     {
-        MockAssignmentLogicSetup(out var mockedAssignmentLogic, _mockFieldCount);
-        var instantiationData = new InstantiationData(mockedAssignmentLogic.Object, _mockFieldCount);
+        MockAssignmentLogicSetup(out var mockedAssignmentLogic, _expectedFieldCount);
+        var instantiationData = new InstantiationData(mockedAssignmentLogic.Object, _expectedFieldCount);
 
         var actualInfo = instantiationData.DataAt(index);
         
@@ -65,10 +65,10 @@ public class InstantiationDataTests
     [Fact]        
     public void DataAt_IndexOutOfRangeException_WithNegative_IndexProvided()
     {
-        MockAssignmentLogicSetup(out var mockedAssignmentLogic, _mockFieldCount);
-        var instantiationData = new InstantiationData(mockedAssignmentLogic.Object, _mockFieldCount);
+        MockAssignmentLogicSetup(out var mockedAssignmentLogic, _expectedFieldCount);
+        var instantiationData = new InstantiationData(mockedAssignmentLogic.Object, _expectedFieldCount);
 
-        void act() => instantiationData.DataAt(-_mockFieldCount);
+        void act() => instantiationData.DataAt(-_expectedFieldCount);
         
         Assert.Throws<IndexOutOfRangeException>(act);        
     }
@@ -76,8 +76,8 @@ public class InstantiationDataTests
     [Fact]        
     public void DataAt_IndexOutOfRangeException_WithGreaterThatCapacity_IndexProvided()
     {
-        MockAssignmentLogicSetup(out var mockedAssignmentLogic, _mockFieldCount);
-        var instantiationData = new InstantiationData(mockedAssignmentLogic.Object, _mockFieldCount);
+        MockAssignmentLogicSetup(out var mockedAssignmentLogic, _expectedFieldCount);
+        var instantiationData = new InstantiationData(mockedAssignmentLogic.Object, _expectedFieldCount);
 
         void act() => instantiationData.DataAt(instantiationData.FieldCount*2);
         
@@ -89,11 +89,11 @@ public class InstantiationDataTests
                                                  int fieldCount = 5)
     {
         mockedAssignmentLogic = new Mock<IFieldAssignmentLogic>();
-        mockedAssignmentLogic.Setup(m => m.Initialize(fieldCount, out _mockedValues, out _mockedTypes))
+        mockedAssignmentLogic.Setup(m => m.Initialize(fieldCount, out _expectedInitializedValues, out _expectedInitializedTypes))
                              .Callback((int s, out IEnumerable<object?> assignedValues, out IEnumerable<Type> providedTypes) => 
                              {
-                                assignedValues = _mockedValues;
-                                providedTypes = _mockedTypes;                                
+                                assignedValues = _expectedInitializedValues;
+                                providedTypes = _expectedInitializedTypes;                                
                              });
     }
 }
